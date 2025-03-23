6e625bcb8c4f10c728de6770d9325859005b9ff0
Martingue <martin.guerandel@gmail.com>
2025-03-23 15:23:33 +0100

    verife color

--- objets.h
+++ objets.h
@@ -1,19 +1,12 @@
 #ifndef objet_simu
 #define objet_simu = 10
 
+#include <SDL2/SDL.h>
 #include "ecran.c"
 #include "objets_maths.h"
 
-typedef struct color
-{
-    int r;
-    int g;
-    int b;
-    int alpha; /*coef de transtarance de naturelement a 255 : pas transparent*/
-} color_t;
-
 typedef struct obj_sph{
-    color_t* c;
+    SDL_Color c;
     sph_t* sph;
 } obj_sph_t;
 
