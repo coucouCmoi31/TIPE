f4bfd5a1bf741cc64ec14bda39d8c85559e46828
Martin <113103857+coucouCmoi31@users.noreply.github.com>
2025-03-24 15:43:23 +0100

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,16 +1,27 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 #include "affichage.h"
+#include "objets_graphique.h"
 
 int MART_SetPixel(SDL_Renderer* renderer, int cx, int cy, SDL_Color color){
     SDL_SetRenderTarget(renderer, NULL);
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_RenderDrawPoint(renderer, cx, cy);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    return 0;
 }
 
+int MART_ColorSphereOnePixel(SDL_Renderer* renderer, obj_sph_t* sph, pt_t* origine, vect_t* direction){
+    return 0;
+}
 
+int Mart_ColorSphere(SDL_Renderer* renderer, obj_sph_t* sph, bloc_ecran_t* e){
+    int h = e->hp * 2;
+    int l = e->lp * 2;
+    for (int i = 0; i < h; i++){
+        for(int j = 0; i < j; j++){
 
+        }
+    }
+    return 0;
+}
