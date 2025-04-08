c205dff930565208f9d92a54a2a0167b289e528c
VeauMignion <148464503+VeauMignion@users.noreply.github.com>
2025-04-08 09:56:14 +0000

    Debut des lights (met des commentaires sur tes fonctions stp)

--- light.h
+++ light.h
@@ -1,1 +1,20 @@
-/*Bla bla*/
+#ifndef LIG
+#define LIG = 10
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+#include "objets_maths.h"
+
+/*toutes les structures sont dans le tas*/
+struct lumiere{
+    float intensité;
+    pt_t position;
+};
+typedef struct lumiere lum_t;
+
+
+
+
+#endif
