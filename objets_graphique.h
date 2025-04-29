18d27e37c83345ec5b4393a04676f7866e36d5c1
Martingue <martin.guerandel@gmail.com>
2025-04-29 11:25:41 +0200

    maj

--- objets_graphique.h
+++ objets_graphique.h
@@ -37,22 +37,31 @@ typedef struct spirit spirit_t;
 /*cree un bloc pour l'ecran d'origine A en de plan de centre B de dimention h et l en px*/
 bloc_ecran_t* sp_ecran(pt_t* A, pt_t* B, int fen_px_h, int fen_px_l);
 
+/* cree un objet sphere en lui atribuant la couleur c */
 obj_sph_t* sp_obj_sph(sph_t* sph, SDL_Color c);
 
+/* cree un objet plan en lui atribuant la couleur c */
 obj_plan_i* sp_obj_plan(plan_i* plan, SDL_Color c);
 
+/* cree un ensemble vide de spirite */
 spirit_t* sp_spirit_vide();
 
+/* augmente la taille en memoire de s */
 void agrandie_taille_max_spirit(spirit_t* s);
 
+/* deplace e selon  (dx, dy, dz) */
 void deplace_ecran(bloc_ecran_t* e, float dx, float dy, float dz);
 
+/* deplace e selon vect normal n, vect largeur l, vect hauteur h */
 void deplace_ecran_vect(bloc_ecran_t* e, float n, float l, float h);
 
+/* deplace e selon vect n */
 void deplace_ecran_semi_vect(bloc_ecran_t* e, float n);
 
+/* tourne l'écran dans la largeur selon teta_rd */
 void rotation_largeur_ecran(bloc_ecran_t*e, float teta_rd);
 
+/* tourne  */
 void rotation_hauteur_ecran(bloc_ecran_t*e, float teta_rd);
 
 void free_obj_shp(obj_sph_t* sphere);
