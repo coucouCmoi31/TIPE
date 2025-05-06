687a42e634a572e3e2bdcba4ebba60d62adf7916
Martingue <martin.guerandel@gmail.com>
2025-05-06 12:37:32 +0200

    free sur les lumi

--- light.c
+++ light.c
@@ -4,13 +4,16 @@
 
 float point_lum_sph(pt_t* point, sph_t* sphe, ch_lum_t* lums, pt_t* camera){
     pt_t* source = lums->tete->light->position;
-    float distance = normale(vect_from_points(point, source));
+    vect_t* v = vect_from_points(point, source);
+    float distance = normale(v);
     float watt_r = lums->tete->light->intensité * (1/(distance*distance));
-    float watt_eff = 255 * watt_r * pro_scal(vect_from_points(point, source),norm_sph(sphe, point));
+    vect_t* n = norm_sph(sphe, point);
+    float watt_eff = 255 * watt_r * pro_scal(v, n);
 
     //FILE* f = fopen("out_lumi.txt", "a");
     //fprintf(f, "-%f-", watt_eff);
     //fclose(f);
-
+    free_vect(v);
+    free_vect(n);
     return watt_eff;
 }
